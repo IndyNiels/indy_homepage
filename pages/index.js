46b39c0a1aaeec6b664dbe533ff64f3bcbf71869
Indy Niels Higuet <indyhiguet94@gmail.com>
2023-11-14 20:21:51 +0100

    changed logo icon

--- pages/index.js
+++ pages/index.js
@@ -37,13 +37,13 @@ const Home = () => (
         bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
         css={{ backdropFilter: 'blur(10px)' }}
       >
-        Hello, I&apos;m an indie app developer based in Japan!
+        Hello, I&apos;m an app developer based in Alicante!
       </Box>
 
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
-            Takuya Matsuyama
+            Indy Niels Higuet
           </Heading>
           <p>Digital Craftsman ( Artist / Developer / Designer )</p>
         </Box>
